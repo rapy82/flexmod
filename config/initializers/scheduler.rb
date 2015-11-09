84d3f4b176c2d8134cdca68f2dc5e7e0ec2b1111
rapy82 <a.rapisarda@netpolarissrl.it>
2015-11-09 19:23:06 +0100

    Decommentato scheduler

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -7,24 +7,24 @@ scheduler.every("30s") do
   #alarms = Alarm.all
 	#FlexmodUtils::check_alarms alarms
 end
-#
-# scheduler.every("20s") do
-#   #FlexmodUtils::check_power_level
-# end
-#
-#
-# #  scheduler.every '30s' do
-# #    puts '***********************************************Fake... Mail'
-# #    AlarmMailer.fake_mail
-# #  end
-#
-#
-# scheduler.every '30s' do
-#   #FlexmodUtils::check_flexmod_connected
-# end
-#
-# scheduler.every("0.3s") do
-#   #p Flexgem::System.read_audio
-# end
+
+scheduler.every("20s") do
+  #FlexmodUtils::check_power_level
+end
+
+
+#  scheduler.every '30s' do
+#    puts '***********************************************Fake... Mail'
+#    AlarmMailer.fake_mail
+#  end
+
+
+scheduler.every '30s' do
+  #FlexmodUtils::check_flexmod_connected
+end
+
+scheduler.every("0.3s") do
+  #p Flexgem::System.read_audio
+end
   
 
